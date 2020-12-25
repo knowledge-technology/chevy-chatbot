0675a535bf4725272decddd46ec9eb587d97e1cb
Barbosa <mbo2@cin.ufpe.br>
2020-12-25 11:41:47 -0300

    fix: get current year and api version

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTheme } from "@react-navigation/native";
 import { View, Image, Linking } from "react-native";
 import { Button, Headline, Subheading, Caption } from "react-native-paper";
+import axios from "axios";
 
 import profileImage from "../../assets/profile.png";
 import data from "../../../package.json";
@@ -10,18 +11,31 @@ import styles from "./styles";
 
 const About = (props) => {
   const { colors } = useTheme();
+  const [apiVersion, setApiVersion] = useState("v1.0.0");
+
+  const getApiVersion = async () => {
+    const response = await axios.get(
+      "http://api.github.com/repos/barbosamaatheus/english-talking-api/tags"
+    );
+    setApiVersion(response.data[0].name);
+  };
+
+  useEffect(() => {
+    getApiVersion();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image style={styles.profile} source={profileImage} />
       <Headline style={{ color: colors.text }}>CHEVY CHATBOT</Headline>
       <Caption style={{ color: colors.textSecondary }}>
-        APP: {data.version} | API: 1.0.0{" "}
+        APP: v{data.version} | API: {apiVersion}
       </Caption>
       <Subheading style={[styles.description, { color: colors.text }]}>
         {data.description}
       </Subheading>
       <Caption style={{ color: colors.textSecondary }}>
-        Developed by Matheus Barbosa © 2020.{" "}
+        Developed by Matheus Barbosa © {new Date().getFullYear()}.
       </Caption>
       <Button
         uppercase={false}
